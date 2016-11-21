b17c1e8bc8c5a6e0257828d64bf0bb6396b90e74
Emmanuel Vasseur <evasseur.ext@orange.com>
2016-11-21 19:57:33 +0100

    Change implementation

--- src/main/java/org/vasseur/battlecode/BattleCode2016.java
+++ src/main/java/org/vasseur/battlecode/BattleCode2016.java
@@ -5,7 +5,7 @@ import org.vasseur.battlecode.IBattleCode2016;
 public class BattleCode2016 implements IBattleCode2016 {
 
     public int sum(int x, int y) {
-        throw new RuntimeException("not implemented yet");
+        return x+y;
     }
 
 }
