442a9cbcb500af87475054954c6c38235f43e14a
Emmanuel Vasseur <vasseur.emmanuel@gmail.com>
2016-12-03 22:40:48 +0100

    Modify implementation in order to test coverage

--- src/main/java/org/vasseur/battlecode/BattleCode2016.java
+++ src/main/java/org/vasseur/battlecode/BattleCode2016.java
@@ -4,8 +4,14 @@ import org.vasseur.battlecode.IBattleCode2016;
 
 public class BattleCode2016 implements IBattleCode2016 {
 
-    public int sum(int x, int y) {
-        return x+y;
-    }
+	public int sum(int x, int y) {
+		if (x == 0) {
+			return y;
+		} else if (y == 0) {
+			return x;
+		} else {
+			return x + y;
+		}
+	}
 
 }
