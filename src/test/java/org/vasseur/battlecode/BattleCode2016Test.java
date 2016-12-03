af84db5070d3ff38eb23f64dfe6de661e674db1f
Emmanuel Vasseur <vasseur.emmanuel@gmail.com>
2016-12-03 22:42:21 +0100

    Add skipped and failed test

--- src/test/java/org/vasseur/battlecode/BattleCode2016Test.java
+++ src/test/java/org/vasseur/battlecode/BattleCode2016Test.java
@@ -2,6 +2,7 @@ package org.vasseur.battlecode;
 
 import static org.fest.assertions.api.Assertions.*;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class BattleCode2016Test {
@@ -11,6 +12,17 @@ public class BattleCode2016Test {
 	@Test
 	public void testSum() {
 		assertThat(battlecode.sum(3,2)).isEqualTo(5);
+    }
+
+	@Test
+	public void badTestSum() {
+		assertThat(battlecode.sum(3,3)).isEqualTo(5);
+    }
+
+	@Test
+	@Ignore
+	public void skippedTestSum() {
+		assertThat(battlecode.sum(3,3)).isEqualTo(6);
     }
 
 }
